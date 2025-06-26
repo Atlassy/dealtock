95c0d51c1fc73a8c2dcbb4337b755677d4f5a365
Atlassy <benaqa@gmail.com>
2025-06-26 05:41:47 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur Dealtock</h1>
+      <p>Application en cours de déploiement avec Netlify & Supabase</p>
+    </div>
+  )
+}
+
+export default App
